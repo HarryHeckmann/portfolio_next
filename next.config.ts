db3673a6f58a117e68a5e9ffc726fbee2976c3bb
Harry <harry@Harrys-MacBook-Air.local>
2025-07-31 22:43:21 -0500

    updates rules

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  "rules": { "react/no-unescaped-entities": 0 }
 };
 
 export default nextConfig;
